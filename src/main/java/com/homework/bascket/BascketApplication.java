3024b7911dca8174fc29b32e43d7f9bf2ccf92e8
SadoALikhan <khan.ali.99.sado@mail.ru>
2025-01-06 21:43:36 +0300

    Init

---
+++ src/main/java/com/homework/bascket/BascketApplication.java
@@ -0,0 +1,13 @@
+package com.homework.bascket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BascketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BascketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homework/bascket/BascketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.bascket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BascketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
